d936ad5535ffc3df695e600a45f509e34eddef4f
JefvdA <jef.v.d.a@live.be>
2022-12-01 11:37:18 +0100

    add initial day01 solution

---
+++ day01/part1.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python3
+"""
+Author: Jef van der Avoirt
+"""
+import os.path
+import pytest
+
+
+INPUT_PATH = os.path.join(os.path.dirname(__file__), "input.txt")
+
+
+def compute(s: str) -> int:
+    calories = []
+    total_calories = []
+    for line in iter(s.splitlines()):
+        if line == "":
+            sum = 0
+            for calorie in calories:
+                sum += calorie
+            total_calories.append(sum)
+            calories = []
+            continue
+        
+        calories.append(int(line))
+    return max(total_calories)
+
+
+@pytest.mark.parametrize(
+    ("sample_input", "expected"),
+    (
+        # put given test cases here
+        ('''\
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000''', 24000),
+    ),
+)
+def test_compute(sample_input: str, expected: int) -> None:
+    assert compute(sample_input) == expected
+
+
+def main():
+    with open(INPUT_PATH) as f:
+        print(compute(f.read()))
+
+
+if __name__ == "__main__":
+    main()
---
+++ day01/part2.py
@@ -0,0 +1,74 @@
+#! /usr/bin/env python3
+"""
+Author: Jef van der Avoirt
+"""
+import os.path
+import pytest
+
+
+INPUT_PATH = os.path.join(os.path.dirname(__file__), "input.txt")
+
+
+def compute(s: str) -> int:
+    calories = []
+    total_calories = []
+    for line in iter(s.splitlines()):
+        if line == "":
+            sum = 0
+            for calorie in calories:
+                sum += calorie
+            total_calories.append(sum)
+            calories = []
+            continue
+        
+        calories.append(int(line))
+    
+    sum = 0
+    for calorie in calories:
+        sum += calorie
+    total_calories.append(sum)
+    calories = []
+    
+    sum = 0
+    for i in range(3):
+        max_calories = max(total_calories)
+
+        sum += max_calories
+
+        total_calories.remove(max_calories)
+
+    return sum
+
+
+@pytest.mark.parametrize(
+    ("sample_input", "expected"),
+    (
+        # put given test cases here
+        ('''\
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000''', 45000),
+    ),
+)
+def test_compute(sample_input: str, expected: int) -> None:
+    assert compute(sample_input) == expected
+
+
+def main():
+    with open(INPUT_PATH) as f:
+        print(compute(f.read()))
+
+
+if __name__ == "__main__":
+    main()
