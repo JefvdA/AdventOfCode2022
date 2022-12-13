90f1416db05b3f70744d1b37a3384078a63e7572
Jef van der Avoirt <jef.v.d.a@live.be>
2022-12-13 22:04:31 +0000

    reset day09

--- day09/part1.py
+++ day09/part1.py
@@ -10,46 +10,7 @@ INPUT_PATH = os.path.join(os.path.dirname(__file__), "input.txt")
 
 
 def compute(s: str) -> int:
-
-    head_position = (0, 0)
-
-    head_positions_list = [head_position]
-
-    for line in s.splitlines():
-        direction, distance = line.split(" ")
-
-        for i in range(int(distance)):
-            match direction:
-                case "U":
-                    head_position = (head_position[0], head_position[1] + 1)
-                case "D":
-                    head_position = (head_position[0], head_position[1] - 1)
-                case "L":
-                    head_position = (head_position[0] - 1, head_position[1])
-                case "R":
-                    head_position = (head_position[0] + 1, head_position[1])
-
-            head_positions_list.append(head_position)
-
-    tail_position_list = calculate_tail_positions(head_positions_list)
-
-    return len(set(tail_position_list))
-
-
-def calculate_tail_positions(head_position_list):
-    tail_position_list = [head_position_list[0]]
-    for head_position in head_position_list:
-        if head_position[1] > tail_position_list[-1][1] + 1:
-            tail_position_list.append((head_position[0], head_position[1] - 1))
-        elif head_position[1] < tail_position_list[-1][1] - 1:
-            tail_position_list.append((head_position[0], head_position[1] + 1))
-        elif head_position[0] > tail_position_list[-1][0] + 1:
-            tail_position_list.append((head_position[0] - 1, head_position[1]))
-        elif head_position[0] < tail_position_list[-1][0] - 1:
-            tail_position_list.append((head_position[0] + 1, head_position[1]))
-    
-    return tail_position_list
-
+    return 0
 
 
 @pytest.mark.parametrize(
