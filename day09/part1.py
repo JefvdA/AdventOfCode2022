c3d5070a45424923b81aaedbe341608a90fe5824
Jef van der Avoirt <jef.v.d.a@live.be>
2022-12-13 22:39:05 +0000

    day09 part1 rework

--- day09/part1.py
+++ day09/part1.py
@@ -9,8 +9,48 @@ import pytest
 INPUT_PATH = os.path.join(os.path.dirname(__file__), "input.txt")
 
 
+DIR_DICTIONARY = {
+    "U": (0, 1),
+    "D": (0, -1),
+    "L": (-1, 0),
+    "R": (1, 0)
+}
+
+
+hx, hy = 0, 0
+tx, ty = 0, 0
+
+
 def compute(s: str) -> int:
-    return 0
+    global tx, ty
+
+    tail_positions = set()
+    tail_positions.add((tx, ty))
+
+    for line in s.splitlines():
+        direction, distance = line.split(" ")
+
+        x, y = DIR_DICTIONARY[direction]
+        for i in range(int(distance)):
+            move(x, y)
+            tail_positions.add((tx, ty))
+
+    return len(tail_positions)
+
+
+def are_touching(x1, y1, x2, y2):
+    return abs(x1 - x2) <= 1 and abs(y1 - y2) <= 1
+
+
+def move(x, y):
+    global hx, hy, tx, ty
+
+    hx += x
+    hy += y
+
+    if (not are_touching(hx, hy, tx, ty)):
+        tx += 0 if hx == tx else (hx - tx) // abs(hx - tx)
+        ty += 0 if hy == ty else (hy - ty) // abs(hy - ty)
 
 
 @pytest.mark.parametrize(
